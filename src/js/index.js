3e7a165001efa84e719dc5e5bd6be67ec7757c9d
Luiz Gustavo Machado <luiz.gustavo.machado@hotmail.com>
2022-11-28 18:39:31 -0300

    startJavaScript

---
+++ src/js/index.js
@@ -0,0 +1,47 @@
+
+const selectTheme = document.querySelector('#select-theme')
+const themeLight = document.querySelector('.light')
+const themeDark = document.querySelector('.dark')
+const selectThemeConteiner = document.querySelector('main.container ')
+
+const buttonPlay = document.querySelector('.play')
+const buttonPause = document.querySelector('.pause')
+const buttonStop = document.querySelector('.stop')
+
+selectTheme.addEventListener('click', function () {
+
+  selectThemeConteiner.classList.toggle('dark-mode')
+  themeLight.classList.toggle('hide')
+  themeDark.classList.toggle('hide')
+})
+
+buttonPlay.addEventListener('click', function () {
+  buttonPlay.classList.remove('hide');
+  buttonPause.classList.add('hide');
+})
+
+buttonPause.addEventListener('click', function () {
+  buttonPause.classList.add('hide');
+  buttonPlay.classList.remove('hide');
+})
+
+buttonStop.addEventListener('click', function () {
+  buttonPlay.classList.remove('hide');
+  buttonPause.classList.add('hide');
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
