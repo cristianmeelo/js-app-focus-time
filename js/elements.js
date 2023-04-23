ad9695d18443d97eead1afd6a0a392745e2307a3
cristianmeelo <cristianmeelo@gmail.com>
2023-04-23 16:57:22 -0300

    :sparkles: feat: added edm

--- js/elements.js
+++ js/elements.js
@@ -1,49 +1,55 @@
-const buttonNormalMode = document.querySelector('.sun')
-const buttonDarkMode = document.querySelector('.moon')
+const buttonNormalMode = document.querySelector(".sun")
+const buttonDarkMode = document.querySelector(".moon")
 
-const buttonPlay = document.querySelector('.play')
-const buttonPause = document.querySelector('.pause')
-const buttonStop = document.querySelector('.stop')
-const buttonIncrease = document.querySelector('.increase')
-const buttonDecrease = document.querySelector('.decrease')
-const buttonForest = document.querySelector('.forest-audio')
-const buttonRain = document.querySelector('.rain-audio')
-const buttonStore = document.querySelector('.store-audio')
-const buttonFire = document.querySelector('.fire-audio')
+const buttonPlay = document.querySelector(".play")
+const buttonPause = document.querySelector(".pause")
+const buttonStop = document.querySelector(".stop")
+const buttonIncrease = document.querySelector(".increase")
+const buttonDecrease = document.querySelector(".decrease")
+const buttonForest = document.querySelector(".forest-audio")
+const buttonRain = document.querySelector(".rain-audio")
+const buttonStore = document.querySelector(".store-audio")
+const buttonFire = document.querySelector(".fire-audio")
+const buttonEdm = document.querySelector(".edm-audio")
 
-const svgForest = document.querySelector('.svg-forest')
-const svgRain = document.querySelector('.svg-rain')
-const svgStore = document.querySelector('.svg-store')
-const svgFire = document.querySelector('.svg-fire')
+const svgForest = document.querySelector(".svg-forest")
+const svgRain = document.querySelector(".svg-rain")
+const svgStore = document.querySelector(".svg-store")
+const svgFire = document.querySelector(".svg-fire")
+const svgEdm = document.querySelector(".svg-edm")
 
-const volForest = document.querySelector('#input-forest')
-const volRain = document.querySelector('#input-rain')
-const volStore = document.querySelector('#input-store')
-const volFire = document.querySelector('#input-fire')
+const volForest = document.querySelector("#input-forest")
+const volRain = document.querySelector("#input-rain")
+const volStore = document.querySelector("#input-store")
+const volFire = document.querySelector("#input-fire")
+const volEdm = document.querySelector("#input-edm")
 
-const minutesDisplay = document.querySelector('.minutes')
-const secondsDisplay = document.querySelector('.seconds')
+const minutesDisplay = document.querySelector(".minutes")
+const secondsDisplay = document.querySelector(".seconds")
 
 export const elements = {
-  buttonNormalMode,
-  buttonDarkMode,
-  buttonPlay,
-  buttonPause,
-  buttonStop,
-  buttonIncrease,
-  buttonDecrease,
-  buttonForest,
-  buttonRain,
-  buttonStore,
-  buttonFire,
-  svgForest,
-  svgRain,
-  svgStore,
-  svgFire,
-  volForest,
-  volRain,
-  volStore,
-  volFire,
-  minutesDisplay,
-  secondsDisplay
+	buttonNormalMode,
+	buttonDarkMode,
+	buttonPlay,
+	buttonPause,
+	buttonStop,
+	buttonIncrease,
+	buttonDecrease,
+	buttonForest,
+	buttonRain,
+	buttonStore,
+	buttonFire,
+	buttonEdm,
+	svgForest,
+	svgRain,
+	svgStore,
+	svgFire,
+	svgEdm,
+	volForest,
+	volRain,
+	volStore,
+	volFire,
+	volEdm,
+	minutesDisplay,
+	secondsDisplay,
 }
--- js/index.js
+++ js/index.js
@@ -1,128 +1,114 @@
-import Sound from './sounds.js'
-import { Timer } from './timer.js'
-import { darkMode } from './theme.js'
-import { elements } from './elements.js'
+import Sound from "./sounds.js"
+import { Timer } from "./timer.js"
+import { darkMode } from "./theme.js"
+import { elements } from "./elements.js"
 
-const {
-  buttonNormalMode,
-  buttonDarkMode,
-  buttonPlay,
-  buttonPause,
-  buttonStop,
-  buttonIncrease,
-  buttonDecrease,
-  buttonForest,
-  buttonRain,
-  buttonStore,
-  buttonFire,
-  svgForest,
-  svgRain,
-  svgStore,
-  svgFire,
-  volForest,
-  volRain,
-  volStore,
-  volFire,
-  minutesDisplay,
-  secondsDisplay
-} = elements
+const { buttonNormalMode, buttonDarkMode, buttonPlay, buttonPause, buttonStop, buttonIncrease, buttonDecrease, buttonForest, buttonRain, buttonStore, buttonFire, buttonEdm, svgForest, svgRain, svgStore, svgFire, svgEdm, volForest, volRain, volStore, volFire, volEdm, minutesDisplay, secondsDisplay } = elements
 
 const timer = Timer({
-  minutesDisplay,
-  secondsDisplay,
-  resetControl
+	minutesDisplay,
+	secondsDisplay,
+	resetControl,
 })
 
 const sound = Sound()
 
 function togglePlay(myAudio) {
-  return myAudio.paused ? myAudio.play() : myAudio.pause()
+	return myAudio.paused ? myAudio.play() : myAudio.pause()
 }
 
 function resetControl() {
-  buttonPlay.classList.remove('hide')
-  buttonPause.classList.add('hide')
+	buttonPlay.classList.remove("hide")
+	buttonPause.classList.add("hide")
 }
 
 /* Button Timer */
 
-buttonPlay.addEventListener('click', () => {
-  buttonPlay.classList.add('hide')
-  buttonPause.classList.remove('hide')
-
-  sound.pressButton()
-
-  timer.countdown()
+buttonPlay.addEventListener("click", () => {
+	buttonPlay.classList.add("hide")
+	buttonPause.classList.remove("hide")
+	sound.pressButton()
+	timer.countdown()
 })
 
-buttonPause.addEventListener('click', () => {
-  resetControl()
-  sound.pressButton()
-  timer.hold()
+buttonPause.addEventListener("click", () => {
+	resetControl()
+	sound.pressButton()
+	timer.hold()
 })
 
-buttonStop.addEventListener('click', () => {
-  resetControl()
-  sound.pressButton()
-  timer.stop()
+buttonStop.addEventListener("click", () => {
+	resetControl()
+	sound.pressButton()
+	timer.stop()
 })
 
-buttonIncrease.addEventListener('click', () => {
-  sound.pressButton()
-  timer.increment()
+buttonIncrease.addEventListener("click", () => {
+	sound.pressButton()
+	timer.increment()
 })
 
-buttonDecrease.addEventListener('click', () => {
-  sound.pressButton()
-  timer.decrement()
+buttonDecrease.addEventListener("click", () => {
+	sound.pressButton()
+	timer.decrement()
 })
 
 /* Button Cards Audio */
 
-buttonForest.addEventListener('click', () => {
-  buttonForest.classList.toggle('selected')
-  svgForest.classList.toggle('color')
-  togglePlay(sound.forestAudio)
+buttonForest.addEventListener("click", () => {
+	buttonForest.classList.toggle("selected")
+	svgForest.classList.toggle("color")
+	togglePlay(sound.forestAudio)
 })
 
-buttonRain.addEventListener('click', () => {
-  buttonRain.classList.toggle('selected')
-  svgRain.classList.toggle('color')
-  togglePlay(sound.rainAudio)
+buttonRain.addEventListener("click", () => {
+	buttonRain.classList.toggle("selected")
+	svgRain.classList.toggle("color")
+	togglePlay(sound.rainAudio)
 })
 
-buttonStore.addEventListener('click', () => {
-  buttonStore.classList.toggle('selected')
-  svgStore.classList.toggle('color')
-  togglePlay(sound.storeAudio)
+buttonStore.addEventListener("click", () => {
+	buttonStore.classList.toggle("selected")
+	svgStore.classList.toggle("color")
+	togglePlay(sound.storeAudio)
 })
 
-buttonFire.addEventListener('click', () => {
-  buttonFire.classList.toggle('selected')
-  svgFire.classList.toggle('color')
-  togglePlay(sound.fireAudio)
+buttonFire.addEventListener("click", () => {
+	buttonFire.classList.toggle("selected")
+	svgFire.classList.toggle("color")
+	togglePlay(sound.fireAudio)
+})
+
+buttonEdm.addEventListener("click", () => {
+	buttonEdm.classList.toggle("selected")
+	svgEdm.classList.toggle("color")
+	togglePlay(sound.edmAudio)
 })
 
 /*  Button Theme */
 darkMode({
-  buttonDarkMode,
-  buttonNormalMode
+	buttonDarkMode,
+	buttonNormalMode,
 })
 
 /* Event Volume */
 
-volForest.addEventListener('input', () => {
-  sound.forestAudio.volume = volForest.value
+volForest.addEventListener("input", () => {
+	sound.forestAudio.volume = volForest.value
 })
 
-volRain.addEventListener('input', () => {
-  sound.rainAudio.volume = volRain.value
+volRain.addEventListener("input", () => {
+	sound.rainAudio.volume = volRain.value
 })
 
-volStore.addEventListener('input', () => {
-  sound.storeAudio.volume = volStore.value
+volStore.addEventListener("input", () => {
+	sound.storeAudio.volume = volStore.value
 })
 
-volFire.addEventListener('input', () => {
-  sound.fireAudio.volume = volFire.value
+volFire.addEventListener("input", () => {
+	sound.fireAudio.volume = volFire.value
+})
+
+volEdm.addEventListener("input", () => {
+	sound.edmAudio.volume = volEdm.value
 })
